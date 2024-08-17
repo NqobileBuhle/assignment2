29cd329f164e7e92c200d0711b956e9e8b4fc43a
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-08-17 23:37:43 +0200

    Edit/Delete updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import MainLayout from './Layout/MainLayout'
 import HomePage from './Pages/HomePage'
 import SeriesPage from './Pages/SeriesPage'
 import Add from './Components/Add'
-import { createNew } from './createNew';
+import EditMovie from './Components/EditMovie'
+// import { createNew } from './createNew';
 
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import MoviesPage from './Pages/MoviesPage'
+import Edit from './Components/EditMovie'
 
 
 const App = () => {
@@ -21,7 +23,9 @@ const App = () => {
         <Route path='/' element={<HomePage/>}></Route> 
         <Route path='/movies' element={<MoviesPage/>}></Route>   
         <Route path='/series' element={<SeriesPage/>}></Route>
-        <Route path="/add" element={<Add  submitMovies={createNew}/>} />    
+        <Route path="/add" element={<Add  submitMovies={createNew}/>} /> 
+        <Route path='/movies/:id' element={<MoviesPage />} /> {/* Add route for movie details */}  
+        <Route path='/edit/:id' element={<EditMovie />} /> {/* Route for details */} 
        </Route>
     )
   )
